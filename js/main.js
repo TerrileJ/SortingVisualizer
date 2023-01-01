62e023fe140604854df78299483d2a64e448644f
Terrile John <terrilejohn101@gmail.com>
2023-01-01 15:12:36 -0800

    Added js for initial bar rendering

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let sortBtn = document.getElementById("sortBtn");
+let genBtn = document.getElementById("genBtn");
+let barContainer = document.getElementById("bars_container");
+
+let min = 1;
+let max = 200;
+let numBars = 20;
+let bar_arr = new Array(numBars);
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function genArray() {
+  for (let i = 0; i < numBars; i++) {
+    bar_arr[i] = randomNumber(min, max);
+  }
+}
+
+function renderArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    let bar = document.createElement("div");
+    bar.classList.add("bar");
+    bar.style.height = `${array[i]}px`;
+    barContainer.appendChild(bar);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  genArray();
+  renderArray(bar_arr);
+});
+/**
+ * 1. Specify a range of values for my array
+ * 2. Make function to create an array of random values
+ */
