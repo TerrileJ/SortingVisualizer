04438b42c6466735072d712637a23e225b4af945
Terrile John <terrilejohn101@gmail.com>
2023-12-05 14:25:59 -0800

    Implemented merge sort with animation, added 2nd area for visualization, increased max speed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let sortBtn = document.getElementById("sortBtn");
 let genBtn = document.getElementById("genBtn");
 let barContainer = document.getElementById("bars_container");
+let barContainerExtra = document.getElementById("bars_container_extra");
 let sortMethodLinks = document.querySelectorAll(".nav-links");
 let bar_slider = document.getElementById("bar-range");
 let speed_slider = document.getElementById("speed-range");
@@ -13,14 +14,14 @@ let bar_arr = new Array(numBars);
 let speed = speed_slider.value / 10.0;
 let sorting = false;
 
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function genArray() {
   for (let i = 0; i < numBars; i++) {
     bar_arr[i] = randomNumber(min, max);
   }
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function renderArray(array) {
@@ -37,10 +38,12 @@ function clear() {
     barContainer.removeChild(barContainer.firstChild);
   }
 }
+
 /* rendering */
 let sort = bubble_sort;
 function selectSort(e) {
   if (sorting) return;
+
   // remove border from any existing elements
   sortMethodLinks.forEach((item) => {
     item.classList.remove("select-border");
@@ -52,7 +55,7 @@ function selectSort(e) {
   // assign the correct sort function
   switch (this.textContent) {
     case "MergeSort":
-      sort = merge_sort;
+      sort = merge_sort_recursive;
       break;
     case "QuickSort":
       break;
@@ -60,22 +63,6 @@ function selectSort(e) {
       break;
     default:
       sort = bubble_sort;
-  }
-}
-
-function sort_freeze(condition) {
-  if (condition) {
-    genBtn.classList.add("disable");
-    sortBtn.classList.add("disable");
-    sortMethodLinks.forEach((item) => {
-      item.classList.add("disable");
-    });
-  } else {
-    genBtn.classList.remove("disable");
-    sortBtn.classList.remove("disable");
-    sortMethodLinks.forEach((item) => {
-      item.classList.remove("disable");
-    });
   }
 }
 
@@ -127,6 +114,23 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+function sort_freeze(condition) {
+  if (condition) {
+    genBtn.classList.add("disable");
+    sortBtn.classList.add("disable");
+    sortMethodLinks.forEach((item) => {
+      item.classList.add("disable");
+    });
+  } else {
+    genBtn.classList.remove("disable");
+    sortBtn.classList.remove("disable");
+    sortMethodLinks.forEach((item) => {
+      item.classList.remove("disable");
+    });
+  }
+}
+
+/** Bubble Sort */
 async function bubble_sort(array, l, r) {
   // conditions for sort animation
   sorting = true;
@@ -169,59 +173,139 @@ async function bubble_sort(array, l, r) {
   sort_freeze(false);
 }
 
-async function merge_sort(array, l, r) {
-  // split into halves and merge
+/** Merge Sort - Iterative (for Animation) */
+
+/** Merge Sort - Recursive (for Records) */
+async function merge_test() {
+  let array = [100, 5, 35, 2, 3, 4, 74, 19, 509, 4329, 5, 3];
+
+  await merge_sort_recursive(array, 0, array.length - 1);
+  console.log(array);
+}
+
+async function merge_sort_recursive(array, l, r) {
+  // allows us to start from subproblem,
   if (l < r) {
-    let m = Math.floor((r - l) / 2) + l;
-    // console.log("L is " + l);
-    // console.log("R is " + r);
-    // console.log("M is " + m);
+    let mid = l + Math.floor((r - l) / 2);
 
-    merge_sort(array, l, m);
-    merge_sort(array, m + 1, r);
+    await merge_sort_recursive(array, l, mid);
+    await merge_sort_recursive(array, mid + 1, r);
 
-    merge(array, l, m, r);
+    await merge_recursive(array, l, mid, r);
   }
 }
 
-async function merge(array, l, m, r, speed) {
-  // create half arrays
+async function merge_recursive(array, l, m, r) {
+  // initialize both halves into their own arrays
   let subArrayOne = new Array(m - l + 1);
   let subArrayTwo = new Array(r - m);
 
   for (let i = 0; i < subArrayOne.length; i++) {
-    subArrayOne[i] = array[l + i];
+    subArrayOne[i] = array[i + l];
   }
 
   for (let i = 0; i < subArrayTwo.length; i++) {
-    subArrayTwo[i] = array[m + 1 + i];
+    subArrayTwo[i] = array[i + m + 1];
   }
 
+  let bars = document.getElementsByClassName("bar");
+  await merge_sort_display_halves(array, bars, l, m, r);
+  let barsExtra = document.getElementsByClassName("bar-extra");
+
+  // go through both sorted halves, sorting them into original array
   let ptrOne = 0;
   let ptrTwo = 0;
+  let kPtr = l;
 
-  // compare values in each half, and then assign smallest to pos in original array
   while (ptrOne < subArrayOne.length && ptrTwo < subArrayTwo.length) {
+    barsExtra[ptrOne].style.backgroundColor = "#203354";
+    barsExtra[subArrayOne.length + ptrTwo].style.backgroundColor = "#203354";
+    await sleep(500 / speed);
+
+    // sort alg
     if (subArrayOne[ptrOne] < subArrayTwo[ptrTwo]) {
-      array[l] = subArrayOne[ptrOne];
+      array[kPtr] = subArrayOne[ptrOne];
+
+      barsExtra[ptrOne].style.backgroundColor = "lightgreen";
+      barsExtra[subArrayOne.length + ptrTwo].style.backgroundColor = "#b0bbc0";
+      await sleep(500 / speed);
+
+      bars[kPtr].style.height = barsExtra[ptrOne].style.height;
+      await sleep(500 / speed);
+
       ptrOne++;
+      kPtr++;
     } else {
-      array[l] = subArrayTwo[ptrTwo];
+      array[kPtr] = subArrayTwo[ptrTwo];
+
+      barsExtra[subArrayOne.length + ptrTwo].style.backgroundColor =
+        "lightgreen";
+      barsExtra[ptrOne].style.backgroundColor = "#b0bbc0";
+      await sleep(500 / speed);
+
+      bars[kPtr].style.height =
+        barsExtra[subArrayOne.length + ptrTwo].style.height;
+      await sleep(500 / speed);
+
       ptrTwo++;
+      kPtr++;
     }
-    l++;
   }
 
-  // assign any remainder values
   while (ptrOne < subArrayOne.length) {
-    array[l] = subArrayOne[ptrOne];
-    l++;
+    bars[kPtr].style.height = barsExtra[ptrOne].style.height;
+    await sleep(500 / speed);
+
+    array[kPtr] = subArrayOne[ptrOne];
     ptrOne++;
+    kPtr++;
   }
 
   while (ptrTwo < subArrayTwo.length) {
-    array[l] = subArrayTwo[ptrTwo];
-    l++;
+    bars[kPtr].style.height =
+      barsExtra[subArrayOne.length + ptrTwo].style.height;
+    await sleep(500 / speed);
+
+    array[kPtr] = subArrayTwo[ptrTwo];
     ptrTwo++;
+    kPtr++;
+  }
+
+  clear_container_extra();
+}
+
+async function merge_sort_display_halves(array, bars, l, m, r) {
+  await display_half(array, bars, l, m); // left half
+  await display_half(array, bars, m + 1, r); // right half
+}
+
+async function display_half(array, bars, start, end) {
+  // highlight left half
+  for (let i = start; i <= end; i++) {
+    bars[i].style.backgroundColor = "#203354";
+  }
+  await sleep(500 / speed);
+
+  // make left half appear in 2nd area
+  for (let i = start; i <= end; i++) {
+    let bar = document.createElement("div");
+    bar.classList.add("bar");
+    bar.classList.add("bar-extra");
+    bar.style.height = `${array[i]}px`;
+    barContainerExtra.appendChild(bar);
+
+    if (i == end) bar.style.marginRight = "150px";
+  }
+  await sleep(500 / speed);
+
+  // Unhighlight left half
+  for (let i = start; i <= end; i++) {
+    bars[i].style.backgroundColor = "#b0bbc0";
+  }
+  await sleep(500 / speed);
+}
+function clear_container_extra() {
+  while (barContainerExtra.firstChild) {
+    barContainerExtra.removeChild(barContainerExtra.firstChild);
   }
 }
